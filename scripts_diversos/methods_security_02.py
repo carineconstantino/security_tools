c9675e46bca216cf89312789bfe7db4b335b8c53
carineconstantino <53983340+carineconstantino@users.noreply.github.com>
2020-03-15 01:05:15 -0300

    Rename methods_security_02.py to scripts_diversos/methods_security_02.py

--- scripts_diversos/methods_security_02.py
+++ scripts_diversos/methods_security_02.py
@@ -3,13 +3,13 @@
 ###############################################################
 #   Repositorio:    Security Tool                             #
 #   Nome:           HTTP Methods Security                     #
-#   DescriÁ„o:      AvaliaÁ„o de SeguranÁa dos mÈtodos HTTP   #
+#   Descri√ß√£o:      Avalia√ß√£o de Seguran√ßa dos m√©todos HTTP   #
 #   Autor:          Carine Constantino                        #            
-#   Vers„o:         1.0                                       #
+#   Vers√£o:         1.0                                       #
 #   Data:           18/09/2019                                #
 #   Python Version: 2.7                                       #
-#   FunÁ„o:         Ferramenta para avaliar a seguranÁa dos   #
-#                   mÈtodos HTTP a partir das URLs informadas #
+#   Fun√ß√£o:         Ferramenta para avaliar a seguran√ßa dos   #
+#                   m√©todos HTTP a partir das URLs informadas #
 #                   no arquivo.txt                            #
 ###############################################################
 #
@@ -30,48 +30,48 @@ for linha in ref_file:
     print (req.url)
     print (req.status_code)
     print ('=============================================\n')
-    print ('An·lise dos mÈtodos permitidos pela aplicaÁ„o\n')
+    print ('An√°lise dos m√©todos permitidos pela aplica√ß√£o\n')
     print ('=============================================')
 
     req = requests.get(linha)
     if req.status_code == 200:
         print 'GET Allow -- Seguro -- Status:', req.status_code, req.reason
     else:
-        print 'GET', req.reason + ' -- Inseguro - Habilite o mÈtodo GET -- Status:', req.status_code
+        print 'GET', req.reason + ' -- Inseguro - Habilite o m√©todo GET -- Status:', req.status_code
 
     req = requests.head(linha)
     if req.status_code == 200:
         print 'HEAD Allow -- Seguro -- Status:', req.status_code, req.reason
     else:
-        print 'HEAD', req.reason + ' -- Esse mÈtodo È opcional -- Status:', req.status_code, req.reason
+        print 'HEAD', req.reason + ' -- Esse m√©todo √© opcional -- Status:', req.status_code, req.reason
 
     req = requests.post(linha)
     if req.status_code == 200:
         print 'POST Allow -- Seguro -- Status:', req.status_code, req.reason
     else:
-        print 'POST', req.reason + ' -- Inseguro - Habilite o mÈtodo POST -- Status:', req.status_code
+        print 'POST', req.reason + ' -- Inseguro - Habilite o m√©todo POST -- Status:', req.status_code
 
     req = requests.put(linha)
     if req.status_code == 200:
-        print 'PUT Allow -- Inseguro - Desabilite o mÈtodo PUT - Status:', req.status_code, req.reason
+        print 'PUT Allow -- Inseguro - Desabilite o m√©todo PUT - Status:', req.status_code, req.reason
     else:
         print 'PUT', req.reason  + ' -- Mantenha o PUT restrito -- Status:', req.status_code
 
     req = requests.delete(linha)
     if req.status_code == 200:
-        print 'DELETE Allow -- Inseguro - Desabilite o mÈtodo DELETE - Staus:', req.status_code, req.reason
+        print 'DELETE Allow -- Inseguro - Desabilite o m√©todo DELETE - Staus:', req.status_code, req.reason
     else:
         print 'DELETE', req.reason + ' -- Mantenha o DELETE restrito -- Status:', req.status_code
 
     req = requests.options(linha)
     if req.status_code == 200:
-        print 'OPTIONS Allow -- Inseguro - Desabilite o mÈtodo OPTIONS - Status:', req.status_code, req.reason
+        print 'OPTIONS Allow -- Inseguro - Desabilite o m√©todo OPTIONS - Status:', req.status_code, req.reason
     else:
         print 'OPTIONS', req.reason + ' -- Mantenha o OPTIONS restrito -- Status:', req.status_code 
         
     req = requests.request('TRACE',linha)
     if req.status_code == 200:
-        print 'TRACE Allow -- Inseguro -- Desabilite o mÈtodo TRACE - Status:', req.status_code, req.reason
+        print 'TRACE Allow -- Inseguro -- Desabilite o m√©todo TRACE - Status:', req.status_code, req.reason
     else:
         print 'TRACE', req.reason + ' -- Mantenha o TRACE restrito -- Status:', req.status_code
 
